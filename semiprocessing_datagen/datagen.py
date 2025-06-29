6cda0204ef069aa72dcb41c03b261aa801369822
CodexForster <dsheka3@uic.edu>
2025-06-29 12:13:21 -0500

    Final contained clusters definition. Fixed XY-midplane coord calc bug.

--- semiprocessing_datagen/datagen.py
+++ semiprocessing_datagen/datagen.py
@@ -39,23 +39,18 @@ def apply_offset(block, offset, rows=13, cols=21):
     
     return new_block
 
-def check_boundary(matrix, threshold=1):
-    """
-    Checks if there are non-zero elements at the boundary of the given matrix.
-    """
-    # Check the boundary elements
-    top_row = matrix[0, :]  # Top row
-    bottom_row = matrix[-1, :]  # Bottom row
-    left_column = matrix[:, 0]  # Left column
-    right_column = matrix[:, -1]  # Right column
+def check_1pix_at_boundary(matrix, threshold=1):
+        # Define the boundary
+        boundaries = np.concatenate([matrix[0, :], matrix[-1, :], matrix[:, 0], matrix[:, -1]])
+        # Check for at least one pixel above the threshold
+        has_pixel_above_threshold = np.any(np.abs(boundaries) > threshold)
+        # Count and sum pixels above the threshold
+        count_above_threshold = np.sum(np.abs(boundaries) > threshold)
+        total_sum = np.sum(boundaries)
 
-    # If any boundary element is non-zero, set the flag to True
-    if (np.any(np.abs(top_row) > threshold) or np.any(np.abs(bottom_row) > threshold) or np.any(np.abs(left_column) > threshold) or np.any(np.abs(right_column) > threshold)    ):
-        return True
-    else:
-        return False
+        return has_pixel_above_threshold, count_above_threshold, total_sum
 
-def split(index,df1,df2,df2_uncentered,df3,df3_uncentered):
+def split(sensor_thickness,index,df1,df2,df2_uncentered,df3,df3_uncentered):
         print("Columns of labels file: ", df1.columns)
         df1.columns = df1.columns.astype(str)
         df2.columns = df2.columns.astype(str)
@@ -65,11 +60,11 @@ def split(index,df1,df2,df2_uncentered,df3,df3_uncentered):
         # unflipped, all charge              
         if("unflipped" not in os.listdir()):
             os.mkdir("unflipped")                                               
-        df1[df1['z-entry']==100].to_parquet("unflipped/labels_d"+str(index)+".parquet")
-        df2[df1['z-entry']==100].to_parquet("unflipped/recon2D_d"+str(index)+".parquet")
-        df2_uncentered[df1['z-entry']==100].to_parquet("unflipped/recon2D_uncentered_d"+str(index)+".parquet")
-        df3[df1['z-entry']==100].to_parquet("unflipped/recon3D_d"+str(index)+".parquet")
-        df3_uncentered[df1['z-entry']==100].to_parquet("unflipped/recon3D_uncentered_d"+str(index)+".parquet")
+        df1[df1['z-entry']==sensor_thickness].to_parquet("unflipped/labels_d"+str(index)+".parquet")
+        df2[df1['z-entry']==sensor_thickness].to_parquet("unflipped/recon2D_d"+str(index)+".parquet")
+        df2_uncentered[df1['z-entry']==sensor_thickness].to_parquet("unflipped/recon2D_uncentered_d"+str(index)+".parquet")
+        df3[df1['z-entry']==sensor_thickness].to_parquet("unflipped/recon3D_d"+str(index)+".parquet")
+        df3_uncentered[df1['z-entry']==sensor_thickness].to_parquet("unflipped/recon3D_uncentered_d"+str(index)+".parquet")
 
 def parseFile(filein,tag,nevents=-1,row_size=13,col_size=21):
         with open(filein) as f:
@@ -137,11 +132,11 @@ def main():
         # row_size, col_size = 13, 21
         maxXshift = 7
         maxYshift = 7
-        boundary_charge_threshold = 1
+        boundary_charge_threshold = 1 # threshold for charge at the boundary to be considered as a boundary charge
         print("==========================")                   
         print(f'NOTE - sensor geometry is hard-coded as {sensor_pitch_X}x{sensor_pitch_Y}x{sensor_thickness} um3. \nAssuming pixel array size = {col_size} X {row_size}/')
         print(f'Offsets introduced = ({maxXshift}, {maxYshift}) along (X, Y).')
-        print("==========================")                   
+        print("==========================")                           
         index = int(sys.argv[1])
         tag = "d"+str(index)
         inputdir = "./"
@@ -152,18 +147,20 @@ def main():
         for col in cols:
                 df[col] = df[col].astype(float)
 
-        df['offset1'] = np.random.randint(-maxYshift, maxYshift+1, size=len(df))
-        df['offset2'] = np.random.randint(-maxXshift, maxXshift+1, size=len(df))
         df['original_atEdge'] = False
         df['uncentered_atEdge'] = False
+        df['chargeOriginal_atEdge'] = 0
+        df['chargeUncentered_atEdge'] = 0
+        df['nPixOriginalAbove1e_atEdge'] = 0
+        df['nPixUncenteredAbove1e_atEdge'] = 0
+        df['offset1'] = np.random.randint(-maxYshift, maxYshift+1, size=len(df))
+        df['offset2'] = np.random.randint(-maxXshift, maxXshift+1, size=len(df))
         # # For shifts (-9, 9) along X and (-5, 5) along Y
         # df['offset1'] = np.random.randint(-5, 6, size=len(df))
         # df['offset2'] = np.random.randint(-9, 10, size=len(df))
         # # For shifts (-7, 7) along X and (-3, 3) along Y
         # df['offset1'] = np.random.randint(-3, 4, size=len(df))
         # df['offset2'] = np.random.randint(-7, 8, size=len(df))
-        df['y-entry'] = df['y-entry'] + df['offset1']*sensor_pitch_Y
-        df['x-entry'] = df['x-entry'] + df['offset2']*sensor_pitch_X
         df['cotAlpha'] = df['n_x']/df['n_z']
         df['cotBeta'] = df['n_y']/df['n_z']
         df['y-midplane'] = df['y-entry'] + df['cotBeta']*(sensor_thickness/2 - df['z-entry'])
@@ -188,7 +185,14 @@ def main():
                 df2list.append(np.array(e[-1]).flatten())
                 matrix = np.array(e[-1])
                 assert matrix.shape[0] == row_size * col_size
-                df.loc[i, 'original_atEdge'] = check_boundary(matrix.reshape(row_size, col_size), boundary_charge_threshold)
+
+                has_pixel_above_threshold_1pix, count_above_threshold_1pix, total_sum_1pix = check_1pix_at_boundary(matrix.reshape(row_size, col_size), boundary_charge_threshold)
+                if has_pixel_above_threshold_1pix:
+                        df.loc[i, 'original_atEdge'] = True
+                        # Following columns are added to the df if has_pixel_above_threshold_1pix is True. Could add them if has_pixel_above_threshold_1pix is False, but would be too small of a charge at the edge to treat as relevant.
+                        df.loc[i, 'chargeOriginal_atEdge'] = total_sum_1pix
+                        df.loc[i, 'nPixOriginalAbove1e_atEdge'] = count_above_threshold_1pix
+
                 df3list.append(np.array(e).flatten())
                 # print("old block:")
                 # for idx, block in enumerate(e.reshape(20, row_size, col_size)):
@@ -196,7 +200,7 @@ def main():
                 #     for row in block:
                 #         print(' '.join(map(str, row)))
                 # All time slices
-                if df.loc[i, 'original_atEdge']: # Exception handling: if cluster is already at edge, then do not apply offsets
+                if has_pixel_above_threshold_1pix: # Exception handling: if cluster is already at edge, then do not apply offsets
                         df.loc[i, 'offset1'] = 0
                         df.loc[i, 'offset2'] = 0
                 random_integer = df['offset1'].iloc[i]
@@ -204,6 +208,12 @@ def main():
                 offset = (random_integer, random_integer2) #(4, 5) #4 is up/down, 5 is left/right
                 offset_values.append(offset)
                 new_blocks = [apply_offset(block, offset, row_size, col_size) for block in e.reshape(20, row_size, col_size)]
+                # Following truth quantities have to be modified to account for the offsets applied to the pixel array.
+                df.loc[i, 'y-entry'] = df.loc[i, 'y-entry'] + df.loc[i, 'offset1']*sensor_pitch_Y
+                df.loc[i, 'x-entry'] = df.loc[i, 'x-entry'] + df.loc[i, 'offset2']*sensor_pitch_X
+                df.loc[i, 'y-midplane'] = df.loc[i, 'y-entry'] + df.loc[i, 'cotBeta']*(sensor_thickness/2 - df.loc[i, 'z-entry'])
+                df.loc[i, 'x-midplane'] = df.loc[i, 'x-entry'] + df.loc[i, 'cotAlpha']*(sensor_thickness/2 - df.loc[i, 'z-entry'])
+                
                 # print("\nNew block:")
                 # for idx, new_block in enumerate(new_blocks):
                 #     print(f'<time slice {idx} ps>')
@@ -212,7 +222,15 @@ def main():
                 df2list_uncentered.append(np.array(new_blocks[-1]).flatten())
                 matrix_uncentered = np.array(new_blocks[-1])
                 assert matrix_uncentered.shape == (row_size, col_size)
-                df.loc[i, 'uncentered_atEdge'] = check_boundary(matrix_uncentered.reshape(row_size, col_size), boundary_charge_threshold)
+                has_pixel_above_threshold_uncentered_1pix, count_above_threshold_uncentered_1pix, total_sum_uncentered_1pix = check_1pix_at_boundary(matrix_uncentered.reshape(row_size, col_size), boundary_charge_threshold)
+                if has_pixel_above_threshold_uncentered_1pix:
+                        df.loc[i, 'uncentered_atEdge'] = True
+                        # Following columns are added to the df if has_pixel_above_threshold_uncentered_1pix is True. Could add them if has_pixel_above_threshold_uncentered_1pix is False, but would be too small of a charge at the edge to treat as relevant.
+                        df.loc[i, 'chargeUncentered_atEdge'] = total_sum_uncentered_1pix
+                        df.loc[i, 'nPixUncenteredAbove1e_atEdge'] = count_above_threshold_uncentered_1pix
+                        if df.loc[i, 'offset1'] == 0 and df.loc[i, 'offset2'] == 0:
+                                assert df.loc[i, 'chargeOriginal_atEdge'] == df.loc[i, 'chargeUncentered_atEdge'], (f"Mismatch: chargeOriginal_atEdge != chargeUncentered_atEdge for 0 offsets applied.")
+                                assert df.loc[i, 'nPixOriginalAbove1e_atEdge'] == df.loc[i, 'nPixUncenteredAbove1e_atEdge'], (f"Mismatch: nPixOriginalAbove1e_atEdge != nPixUncenteredAbove1e_atEdge for 0 offsets applied.")
                 df3list_uncentered.append(np.array(new_blocks).flatten())
         df2 = pd.DataFrame(df2list)
         df2_uncentered = pd.DataFrame(df2list_uncentered)
@@ -220,7 +238,7 @@ def main():
         df3_uncentered = pd.DataFrame(df3list_uncentered)  
 
         # split into flipped/unflipped, pos/neg charge
-        split(index,df,df2,df2_uncentered,df3,df3_uncentered)
+        split(sensor_thickness,index,df,df2,df2_uncentered,df3,df3_uncentered)
         offsets_x = [offset[0] for offset in offset_values]
         offsets_y = [offset[1] for offset in offset_values]
 
